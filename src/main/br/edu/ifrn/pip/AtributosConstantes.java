c06dd002f66d5afc7490b928e06bbfd29169e11a
Welkson Renny de Medeiros <welkson.medeiros@ifrn.edu.br>
2018-06-08 08:49:03 -0300

    Arquivo de constantes

---
+++ src/main/br/edu/ifrn/pip/AtributosConstantes.java
@@ -0,0 +1,10 @@
+package br.edu.ifrn.pip;
+
+public class AtributosConstantes {
+	public static final String DOMINIO_BASE = "http://ifrn.edu.br/";
+	
+	public static final String ATRIB_USUARIO_DEPARTAMENTO = "http://ifrn.edu.br/usuario/departamento";
+
+	public static final String ATRIB_CENTRALSERV_DONOTICKET = "http://ifrn.edu.br/centralservicos/donoticket";
+	
+}
