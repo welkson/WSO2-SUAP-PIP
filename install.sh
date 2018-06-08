e3bcbd36161f50b13d625e9a3a8919e59c38edf5
Welkson Renny de Medeiros <welkson.medeiros@ifrn.edu.br>
2018-06-08 16:43:22 -0300

    Script de instalacao

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
+WSO_DIR="/Users/welkson/wso2is-5.4.0-update2"
+LIB_DIR="$WSO_DIR/repository/components/lib"
+
 echo "\n\n[*] Copiando jar..."
-cp target/WSO2-SUAP-PIP-1.0.1.jar /Users/welkson/wso2is-5.4.0-update2/repository/components/lib
+cp target/WSO2-SUAP-PIP-1.0.1.jar $WSO_DIR/repository/components/lib
 
 echo "\n[*] Listando pasta lib do WSO2..."
-ls -lah /Users/welkson/wso2is-5.4.0-update2/repository/components/lib
+ls -lah $LIB_DIR
 
 echo "\n[*] Listando configuracao do atributo no entitlement.properties do WSO2..."
-cat ~/wso2is-5.4.0-update2/repository/conf/identity/entitlement.properties | grep ifrn
+cat $WSO_DIR/repository/conf/identity/entitlement.properties | grep ifrn
 
 echo "\n\nINSTALAÇÃO CONCLUÍDA! (Reinicie o WSO2)"
